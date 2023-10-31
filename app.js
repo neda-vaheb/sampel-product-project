2a1a802fe4ba85cd3d557873af4bc368579c33ef
Neda Vaheb <n.vaheb.nv@gmail.com>
2023-10-31 08:14:55 -0700

    java script related to buttons is complete

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const searchInput = document.querySelector("#search-input");
 const products = document.querySelectorAll(".product-item");
+const buttons = document.querySelectorAll(".filter");
+
+const selectedButtton=(filter)=>{
+
+    buttons.forEach((button)=>{
+        const filterBtn = button.dataset.filter;
+        (filter === filterBtn) ? button.classList.add("selected") : button.classList.remove("selected");
+
+    })
+}
 
 function keyHandler(event){
-
 const searchValue = event.target.value.toLowerCase().trim();
-
 products.forEach ((product)=>{
    const productName = product.children[1].innerText.toLowerCase();
-  
    if(productName.includes(searchValue)){
     product.style.display = "block";
    }else{
@@ -17,6 +24,25 @@ products.forEach ((product)=>{
 
 }
 
+const buttonHandler = (event) =>{
+    const filter = event.target.dataset.filter ; 
+    selectedButtton(filter);
+    products.forEach((product)=>{
+       const category = product.dataset.category;
+       if(category === "all"){
+        product.style.display = "block";
+       }else{
+        (category === filter) ? product.style.display = "block" :  product.style.display = "none"; 
+       }
+
+
+    })
+
+
+}
+
 
 searchInput.addEventListener("keyup" , keyHandler);
-
+buttons.forEach((button)=>{
+    button.addEventListener("click" , buttonHandler);
+})
